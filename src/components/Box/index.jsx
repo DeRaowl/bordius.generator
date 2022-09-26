8374180917d7a7a8f75663aaaf6945c6c98f1652
Rahul M <rahulm@ip-192-168-1-8.ec2.internal>
2022-09-26 23:11:55 +0530

    Refactor styles

--- src/components/Box/index.jsx
+++ src/components/Box/index.jsx
@@ -1,10 +1,15 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import Slider from '../Slider';
-import _debounce from 'lodash/debounce';
 import './index.css';
+import { FiCopy } from 'react-icons/fi';
 
 function Box() {
-  const [positions, setPositions] = useState({ top: 30, right: 30, bottom: 30, left: 30 });
+  const [positions, setPositions] = useState({
+    top: 30,
+    right: 30,
+    bottom: 30,
+    left: 30
+  });
   const [isCopied, setIsCopied] = useState(false);
 
   const onRangeChange = (event) => {
@@ -12,7 +17,6 @@ function Box() {
     setPositions({ ...positions, [name]: value });
   };
 
-  const debounceFn = useCallback(_debounce(onRangeChange, 500), []);
   const { top, right, bottom, left } = positions;
   const borderRadius = `${top}% ${100 - top}% ${100 - bottom}% ${bottom}% / ${left}% ${right}% ${
     100 - right
@@ -23,12 +27,13 @@ function Box() {
   top-left-left top-right-right bottom-right-right bottom-left-left;
    */
   const boxStyle = {
-    width: '450px',
-    height: '450px',
+    width: '300px',
+    height: '300px',
     borderRadius,
     transform: 'translate 0.2s ease',
-    background: '#272727',
-    boxShadow: '-200px 100px rgba(200,200,200,0.1)'
+    backgroundImage:
+      'linear-gradient(70deg,hsl(240deg 100% 20%) 0%,hsl(289deg 100% 21%) 6%,hsl(315deg 100% 27%) 15%,hsl(329deg 100% 36%) 28%,hsl(337deg 100% 43%) 46%,hsl(357deg 91% 59%) 65%,hsl(17deg 100% 59%) 78%,hsl(34deg 100% 53%) 88%,hsl(45deg 100% 50%) 95%,hsl(55deg 100% 50%) 100%)',
+    boxShadow: '-100px 75px rgba(255,255,255,0.3)'
   };
 
   const copyToClipBoard = () => {
@@ -44,7 +49,13 @@ function Box() {
   }, [isCopied]);
 
   return (
-    <>
+    <div className="main">
+      <div className="header">
+        <p className="header__title">Bordius Generator</p>
+        <span role="img" aria-label="spark" className="header__desc">
+          A perfect border radius ✨
+        </span>
+      </div>
       <section className="box">
         <div className="box__wrapper" style={boxStyle}></div>
         <div className="sliders">
@@ -54,18 +65,19 @@ function Box() {
           <Slider type="range" className="slider left" name="left" onChange={onRangeChange} />
         </div>
       </section>
-      <div className="user__content">
-        <span className="title"> Border-Radius :</span>{' '}
-        <span className="actual__output">
+      <div className="content">
+        <p className="content__actual">
+          <span>border-radius: </span>
           {`${top}% ${100 - top}% ${100 - bottom}% ${bottom}% / ${left}% ${right}% ${
             100 - right
           }% ${100 - left}%`}
-        </span>
-        <span className="clipboard" onClick={copyToClipBoard}>
-          {isCopied ? 'Copied' : 'Copy to Clipboard'}
-        </span>
+        </p>
+        <p className="content__clipboard" onClick={copyToClipBoard}>
+          <span>{<FiCopy />}</span>
+          {isCopied ? 'Copied' : 'Copy'}
+        </p>
       </div>
-    </>
+    </div>
   );
 }
 
